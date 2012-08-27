dde2b02b936ade2731905cf38e8aa59a9eacd89c
Josh Rolstad <jrolstad@gmail.com>
2012-08-27 06:53:02 -0700

    Cleaning up so can run junit tests

--- src/campreview/android/ui/activities/HomeActivity.java
+++ src/campreview/android/ui/activities/HomeActivity.java
@@ -3,18 +3,3 @@ package campreview.android.ui.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import campreview.android.R;
-
-/**
- * Created with IntelliJ IDEA.
- * User: rolstad
- * Date: 6/25/12
- * Time: 9:56 PM
- * To change this template use File | Settings | File Templates.
- */
-public class HomeActivity extends Activity {
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-}
